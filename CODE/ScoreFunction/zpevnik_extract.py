40d4d5ba1831f4ba78eeea69f959c1e05692fe70
Barbora Stepankova <barca-stepankova@seznam.cz>
2024-01-25 18:38:12 +0100

    Update Zpevnik extract function.

--- CODE/ScoreFunction/zpevnik_extract.py
+++ CODE/ScoreFunction/zpevnik_extract.py
@@ -1,11 +1,23 @@
 import re
 import math
+from langdetect import detect_langs
 from phoneme_repetition_similarity import phoneme_distinct2
 
 
-MAX_LINES = 10
-MIN_LINES = 2
+MAX_LINES = 8
+MIN_LINES = 3
 
+MAX_LINE_LEN = 55
+TRY_SPLIT_LINE_LEN = 25
+MIN_LINE_LEN = 5
+
+def detect_language_with_langdetect(line): 
+    try: 
+        langs = detect_langs(line) 
+        for item in langs: 
+            # The first one returned is usually the one that has the highest probability
+            return item.lang, item.prob 
+    except: return "err", 0.0 
 
 def split_section_based_on_distinct2(temp_section : list[str], min_lines : int = 2) -> tuple[list[str]]:
     """
@@ -119,14 +131,16 @@ with open("DATA\\Velky_zpevnik\\velkyzpevnik.txt", "r", encoding="utf-8") as fil
 
 deletions=[
     r"\bR\b",
+    r"\br\b",
+    r"\brf\b",
     r"\bRef\b",
     r"\bREF\b",
     r"\bref\b",
     r"\bRefrén\b",
     r"\bRefrain\b",
-    r"\b[CDEFGHB][bs#]*\b",
-    r"\b[CDEFGHB]mi\b",
-    r"\b[CDEFGHB]é\b",
+    r"\b[CDEFGHBXxcdefghb][bs#]*\b",
+    r"\b[CDEFGHBXxcdefghb]mi\b",
+    r"\b[CDEFGHBXxcdefghb][éá]\b",
     r"[0-9]+"
         ]
 
@@ -139,14 +153,19 @@ last_section_short = False
 sum_lines = 0
 avgs_letters = 0
 
-for line in lines:
+for i in range(len(lines)):
     for d in deletions: 
-        line=re.sub(d," ",line)
+        lines[i] = re.sub(d," ",lines[i])
 
-    depunctuised_line = re.sub(r"[.:…,;?-]", "", line)
+    line = re.sub(r'[^a-zA-Z\sěščřžýáíéúůóťďňĚŠČŘŽÝÁÍÉÚŮÓŤĎŇ,.()]+', ' ', lines[i])
+    line = re.sub(r'\s+', ' ', line)
 
-    if not depunctuised_line.strip() and len(temp_section) > 0:    # if the line contains just white spaces
+    if not re.sub(r"[.,()]", '', line).strip() and len(temp_section) > 0:    # if the line contains just white spaces
         # print("lines in a section: ", len(temp_section), "index: ", counter)
+
+        lang, prob = detect_language_with_langdetect(" ".join(temp_section))
+        if lang != "cs" and lang != "sk" and lang != "sl":
+            continue
 
         if len(temp_section) > MAX_LINES:
             if last_section_short:
@@ -172,35 +191,55 @@ for line in lines:
                 sum_lines += len(temp_section)
 
         temp_section.clear()
-        print(counter)
-        print("avg lines in section:", sum_lines / counter)
+        print(counter - 1)
+        print("avg lines in section:", sum_lines / (counter - 1))
         print("letters/line:", avgs_letters / sum_lines)
         
 
-    if depunctuised_line.strip():
-        while len(line) > 0 and re.match(r"[.:,?;-]|\s", line[-1]):
+    if re.sub(r"[.,()]", '', line).strip():
+        while len(line) > 0 and re.match(r'[\s,.()]+', line[-1]):
             line = line[:-1]
+        while re.match(r'[\s,.()]+', line[0]):
+            line = line[1:]
 
         avgs_letters += len(line)
 
-        if len(line) > 25:
-            split_line : list[str] = re.split(r"[.:,?;-]", line)
-            for line_part in split_line:
-                if not line_part.strip():       # there are empty line parts
+        if len(line) > TRY_SPLIT_LINE_LEN:
+            split_line : list[str] = re.split(r"[.,()]", line)
+            cleaned_split_line = []
+            for split_i in range(len(split_line)):
+                if not split_line[split_i].strip():       # there are empty line parts
                     continue
 
-                while re.match(r"[.:,?;-]|\s", line_part[0]):
-                    line_part = line_part[1:]
+                while re.match(r'[\s,.()]+', split_line[split_i][0]):
+                    split_line[split_i] = split_line[split_i][1:]
+                while re.match(r'[\s,.()]+', split_line[split_i][-1]):
+                    split_line[split_i] = split_line[split_i][:-1]
 
-                temp_section.append(line_part + "\n")
-                # if len(line_part) != len(line):
-                #     print("   ", len(line_part))
+                cleaned_split_line.append(split_line[split_i])
+            
+            for split_i in range(len(cleaned_split_line)):
+                line_part = cleaned_split_line[split_i]
+
+                if len(line_part) > MAX_LINE_LEN:
+                    pass
+                elif len(line_part) < MIN_LINE_LEN:
+                    if split_i + 1 < len(cleaned_split_line):   # there is a following split section
+                        if len(line_part) + len(cleaned_split_line[split_i + 1]) + 2 < MAX_LINE_LEN:    # including ", " it is short enough
+                            cleaned_split_line[split_i + 1] = line_part + ", " + cleaned_split_line[split_i + 1]
+                else:
+                    temp_section.append(line_part + "\n")
+
+        elif len(line) < 5:
+            pass
 
         else:
-            while re.match(r"[.:,?;-]|\s", line[0]):
-                    line = line[1:]
+            while re.match(r'[\s,.()]+', line[0]):
+                line = line[1:]
+            while re.match(r'[\s,.()]+', line[-1]):
+                line = line[:-1]
 
-            temp_section.append(line + "\n")
+            temp_section.append(re.sub(r"[()]", '', line) + "\n")
 
 
 
