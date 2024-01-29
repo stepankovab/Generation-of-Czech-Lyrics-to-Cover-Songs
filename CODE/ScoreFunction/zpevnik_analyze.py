8c51cbb1d495dc64b9290469b26c0787717580aa
Barbora Stepankova <barca-stepankova@seznam.cz>
2024-01-29 20:45:26 +0100

    Update VZ GT generation/ analysis

---
+++ CODE/ScoreFunction/zpevnik_analyze.py
@@ -0,0 +1,29 @@
+from nltk.tokenize import word_tokenize
+import lzma
+import pickle
+
+# word_count_dict = {}
+
+# with open("DATA\\Velky_zpevnik\\velkyzpevnik.txt", "r", encoding="utf-8") as file:
+#     lines = file.readlines()
+
+# for line in lines:
+#     line_words = word_tokenize(line, "czech")
+#     for word in line_words:
+#         if word in word_count_dict:
+#             word_count_dict[word] += 1
+#         else:
+#             word_count_dict[word] = 1
+
+# sorted_word_count = sorted(word_count_dict.items(), key=lambda x:x[1], reverse=True)
+# sorted_word_count_dict = dict(sorted_word_count)
+
+# # Serialize the model.
+# with lzma.open("DATA/Velky_zpevnik/OG_file_analysis.dict", "wb") as model_file:
+#     pickle.dump(sorted_word_count_dict, model_file)
+
+
+with lzma.open("DATA/Velky_zpevnik/OG_file_analysis.dict", "rb") as model_file:
+    sorted_word_count_dict = pickle.load(model_file)
+
+print("a")
--- CODE/ScoreFunction/zpevnik_append_truth.py
+++ CODE/ScoreFunction/zpevnik_append_truth.py
@@ -5,6 +5,22 @@ from sentence_transformers import SentenceTransformer
 import Imports.tagger as tagger
 from syllabator import syllabify
 
+def fill_in_none_rhymes(rhymes : list[int|None]) -> list[int]:
+    max_rhyme_ref = 0
+    none_ids = []
+    for rhyme_i in range(len(rhymes)):
+        if isinstance(rhymes[rhyme_i], int):
+            if rhymes[rhyme_i] > max_rhyme_ref:
+                max_rhyme_ref = rhymes[rhyme_i]
+        else:
+            none_ids.append(rhyme_i)
+
+    for none_i in none_ids:
+        max_rhyme_ref += 1
+        rhymes[none_i] = max_rhyme_ref
+
+    return rhymes
+
 
 directory = 'DATA/Velky_zpevnik/VZ_sections'
 
@@ -26,6 +42,8 @@ for filename in os.listdir(directory):
 
     rhymes = rt.tag(poem=lyrics_section, output_format=3)
 
+    rhymes = fill_in_none_rhymes(rhymes)
+
     for i in range(len(lyrics_section)):
         lines_info.append((len(syllabify(lyrics_section[i], "cs")), rhymes[i]))
     
@@ -38,8 +56,10 @@ for filename in os.listdir(directory):
 
     embedding = model.encode(en_lyrics_joined, convert_to_numpy=True)
 
+    print(filename)
+
     with open("DATA/Velky_zpevnik/VZ_prompted_sections/GT_" + filename, "w", encoding="utf-8") as new_section:
-        new_section.write(str(lines_count) + " , " + " , ".join([str(l) + " " + str(r) for (l, r) in lines_info]) + " , " + re.sub("\n", "", str(embedding)))
+        new_section.write(str(lines_count) + "," + ",".join([str(l) + " " + str(r) for (l, r) in lines_info]) + "," + re.sub("\n", "", str(embedding)))
         new_section.write("\n\n-----\n\n")
         new_section.writelines(lyrics_section)
         
