17698300235d394563e243512ba3f8488189a747
Barbora Stepankova <barca-stepankova@seznam.cz>
2024-03-22 01:21:06 +0100

    rewrite to generate line by line

--- CODE/LLMExperiments/generate_lines.py
+++ CODE/LLMExperiments/generate_lines.py
@@ -4,19 +4,20 @@ from dataset_types import DatasetType
 from english_structure_extractor import SectionStructure
 from CODE.LLMExperiments.evaluator import Evaluator
 from eval.syllabator import syllabify
+import os
+import requests
 
 
 class StoppingSequenceCriteria(StoppingCriteria):
     def __init__(self, prompt, tokenizer):
         self.prompt=prompt
         self.tokenizer = tokenizer
-        self.number_of_lines = len(prompt.split("#")[0].strip().split(" "))
 
     def __call__(self, input_ids, scores, **kwargs):
         generated_text = self.tokenizer.decode(input_ids[0])
         generated_text = generated_text.replace(self.prompt,'')
         generated_lines = generated_text.strip().split("\n")
-        if (len(generated_lines) == self.number_of_lines and len(syllabify(generated_lines[-1].split("#")[-1].strip())) >= int(self.prompt.split("#")[0].strip().split(" ")[-1])) or len(generated_lines) > self.number_of_lines:
+        if (len(generated_lines) > len(self.prompt.strip().split("\n"))):
             return True  # Stop generation
         return False  # Continue generation
 
@@ -26,97 +27,173 @@ class StoppingSequenceCriteria(StoppingCriteria):
     def __iter__(self):
         yield self
 
+def prepare_prompt(dataset_type, structure: SectionStructure, line_i, ending = None):
+    if dataset_type == DatasetType.CHARACTERISTIC_WORDS:
+        prompt = f"{structure.line_keywords[line_i]} # "
+    elif dataset_type == DatasetType.ENDS_AND_WORDS:
+        prompt = f"{structure.line_keywords[line_i]} #\n{ending} # "
+    elif dataset_type == DatasetType.BASELINE:
+        prompt = ""
+    elif dataset_type == DatasetType.END_OF_LINES:
+        prompt = f"{ending} # "
+    elif dataset_type == DatasetType.SYLLABLES:
+        prompt = f"{structure.syllables[line_i]} # "
+    elif dataset_type == DatasetType.SYLLABLES_AND_ENDS:
+        prompt = f"{structure.syllables[line_i]} # {ending} # "
+    elif dataset_type == DatasetType.SYLLABLES_AND_WORDS:
+        prompt = f"{structure.line_keywords[line_i]} #\n{structure.syllables[line_i]} # "
+    elif dataset_type == DatasetType.SYLLABLES_ENDS_WORDS:
+        prompt = f"{structure.line_keywords[line_i]} #\n{structure.syllables[line_i]} # {ending} # "
+    elif dataset_type == DatasetType.UNRHYMED_LEN:
+        url = 'http://lindat.mff.cuni.cz/services/translation/api/v2/models/en-cs'
+        response = requests.post(url, data = {"input_text": structure.original_lyrics[line_i]})
+        response.encoding='utf8'
+        translated_output = response.text[:-1]
+        prompt = f"{len(syllabify(translated_output))} # {translated_output} #\n{structure.syllables[line_i]} # "
+    elif dataset_type == DatasetType.UNRHYMED_LEN_END:
+        url = 'http://lindat.mff.cuni.cz/services/translation/api/v2/models/en-cs'
+        response = requests.post(url, data = {"input_text": structure.original_lyrics[line_i]})
+        response.encoding='utf8'
+        translated_output = response.text[:-1]
+        translated_output_sylls = syllabify(translated_output)
+        prompt = f"{len(translated_output_sylls)} # {translated_output_sylls[-1][-min(len(translated_output_sylls[-1]), 3):]} # {translated_output} #\n{structure.syllables[line_i]} # {ending} # "
+    else:
+        prompt = ""
 
-def generate_lines(model, model_path, tokenizer, input_sections, dataset_type, device="cpu"):
-    model.load_state_dict(state_dict=torch.load(model_path, map_location=torch.device(device)))
-    model.eval()
+    return prompt
+
+
+def generate_lines(args, input_sections):
+
+    LINES_PER_GENERATION = 5
+
+    wout_dataset_type = DatasetType(args.dataset_type)
+
+    if wout_dataset_type == DatasetType.CHARACTERISTIC_WORDS:
+        w_dataset_type = DatasetType.ENDS_AND_WORDS
+    elif wout_dataset_type == DatasetType.BASELINE:
+        w_dataset_type = DatasetType.END_OF_LINES
+    elif wout_dataset_type == DatasetType.SYLLABLES:
+        w_dataset_type = DatasetType.SYLLABLES_AND_ENDS
+    elif wout_dataset_type == DatasetType.SYLLABLES_AND_WORDS:
+        w_dataset_type = DatasetType.SYLLABLES_ENDS_WORDS
+    elif wout_dataset_type == DatasetType.UNRHYMED_LEN:
+        w_dataset_type = DatasetType.UNRHYMED_LEN_END
+    else:
+        raise Exception(f"We don't support a Dataset type {wout_dataset_type}")
+
+    device = 'cpu'
+    if torch.cuda.is_available():
+        print("cuda available.")
+        device = 'cuda'
+
+    if args.model == "GPT2_oscar":
+        tokenizer = AutoTokenizer.from_pretrained("lchaloupsky/czech-gpt2-oscar")
+        wout_model = AutoModelForCausalLM.from_pretrained("lchaloupsky/czech-gpt2-oscar")
+        w_model = AutoModelForCausalLM.from_pretrained("lchaloupsky/czech-gpt2-oscar")
+        tokenizer.model_max_length=1024
+
+    elif args.model == "GPT2_czech_XL":
+        tokenizer = AutoTokenizer.from_pretrained("BUT-FIT/Czech-GPT-2-XL-133k")
+        wout_model = AutoModelForCausalLM.from_pretrained("BUT-FIT/Czech-GPT-2-XL-133k")
+        w_model = AutoModelForCausalLM.from_pretrained("BUT-FIT/Czech-GPT-2-XL-133k")
+        tokenizer.model_max_length=1024
+
+    elif args.model == "Mistral_czech":
+        tokenizer = AutoTokenizer.from_pretrained("simecek/cswikimistral_0.1")
+        wout_model = AutoModelForCausalLM.from_pretrained("simecek/cswikimistral_0.1")
+        w_model = AutoModelForCausalLM.from_pretrained("simecek/cswikimistral_0.1")
+        tokenizer.model_max_length=1024
+
+    elif args.model == "tinyLlama":
+        tokenizer = AutoTokenizer.from_pretrained("TinyLlama/TinyLlama-1.1B-intermediate-step-1431k-3T")
+        wout_model = AutoModelForCausalLM.from_pretrained("TinyLlama/TinyLlama-1.1B-intermediate-step-1431k-3T")
+        w_model = AutoModelForCausalLM.from_pretrained("TinyLlama/TinyLlama-1.1B-intermediate-step-1431k-3T")
+        tokenizer.model_max_length=1024
+
+    wout_model_path = os.path.join(args.dataset_path, "trained_models", f"{args.model}_{wout_dataset_type.name}_one_line_lyricist_{args.epoch}.pt")
+    w_model_path = os.path.join(args.dataset_path, "trained_models", f"{args.model}_{w_dataset_type.name}_one_line_lyricist_{args.epoch}.pt")
+    
+    print("="*10 + "  " + wout_model_path + " " + "="*10)
+    wout_model.load_state_dict(state_dict=torch.load(wout_model_path, map_location=torch.device(device)))
+    w_model.load_state_dict(state_dict=torch.load(w_model_path, map_location=torch.device(device)))
+    wout_model.eval()
+    w_model.eval()
+
+    structure = SectionStructure()
+    evaluator = Evaluator()
 
     for input_section in input_sections:
         # Load the structure of the english text
-        og_structure = SectionStructure()
-        og_structure.fill(input_section) 
+        structure.fill(input_section) 
 
-        if dataset_type == DatasetType.BASELINE:
-            prompt = ""
+        temp_result = [[] for x in range(LINES_PER_GENERATION)]
 
-        elif dataset_type == DatasetType.SYLLABLES:
-            prompt = f"{' '.join([str(x) for x in og_structure.syllables])} #\n"
-                    
-        elif dataset_type == DatasetType.END_OF_LINES:
-            prompt = ""
+        known_endings = {}
 
-        elif dataset_type == DatasetType.CHARACTERISTIC_WORDS:
-            prompt = f"{og_structure.num_lines} # {' '.join(og_structure.keywords)} #\n"
+        for line_i in range(structure.num_lines):
 
-        elif dataset_type == DatasetType.UNRHYMED_LEN:
-            pass
+            if structure.rhyme_scheme[line_i] in known_endings:
+                prompt = prepare_prompt(w_dataset_type, structure, line_i, known_endings[structure.rhyme_scheme[line_i]])
 
-        elif dataset_type == DatasetType.SYLLABLES_AND_WORDS:
-            prompt = f"{' '.join([str(x) for x in og_structure.syllables])} # {' '.join(og_structure.keywords)} #\n"
+                print(prompt)
+                inputs = tokenizer(prompt, return_tensors="pt") 
+                tokenizer.encode(prompt, return_tensors="pt")
+        
+                # model output using Top-k sampling text generation method
+                sample_outputs = w_model.generate(**inputs,
+                    do_sample=True,
+                    pad_token_id=tokenizer.eos_token_id,
+                    num_return_sequences=LINES_PER_GENERATION,
+                    penalty_alpha=0.6,
+                    max_new_tokens=512,
+                    stopping_criteria=StoppingSequenceCriteria(prompt, tokenizer),
+                    )
 
-        elif dataset_type == DatasetType.SYLLABLES_AND_ENDS:
-            prompt = f"{' '.join([str(x) for x in og_structure.syllables])} # ví nám ní ky #\n"
+                # generated sequence
+                for i, sample_output in enumerate(sample_outputs):
+                    model_out = tokenizer.decode(sample_output.tolist(), skip_special_tokens=True)
+                    print("\n{}\n\n{}\n".format(i+1, model_out)) # tokenizer.decode(sample_output, skip_special_tokens=True)
+                    temp_result[i].append(model_out.strip().split("#")[-1])
 
-        elif dataset_type == DatasetType.ENDS_AND_WORDS:
-            prompt = f"ví nám tel sám # {' '.join(og_structure.keywords)} #\n"
+            else:
+                prompt = prepare_prompt(wout_dataset_type, structure, line_i)
 
-        elif dataset_type == DatasetType.FORCED_SYLLABLES:
-            prompt = f"{' '.join([str(x) for x in og_structure.syllables])} #\n"
+                print(prompt)
+                inputs = tokenizer(prompt, return_tensors="pt") 
+                tokenizer.encode(prompt, return_tensors="pt")
+        
+                # model output using Top-k sampling text generation method
+                sample_outputs = wout_model.generate(**inputs,
+                    do_sample=True,
+                    pad_token_id=tokenizer.eos_token_id,
+                    num_return_sequences=LINES_PER_GENERATION,
+                    penalty_alpha=0.6,
+                    max_new_tokens=512,
+                    stopping_criteria=StoppingSequenceCriteria(prompt, tokenizer),
+                    )
 
-        else:
-            raise Exception(f"We don't support a Dataset type {dataset_type}")
+                # generated sequence
+                for i, sample_output in enumerate(sample_outputs):
+                    model_out = tokenizer.decode(sample_output.tolist(), skip_special_tokens=True)
+                    print("\n{}\n\n{}\n".format(i+1, model_out)) # tokenizer.decode(sample_output, skip_special_tokens=True)
+                    temp_result[i].append(model_out.strip().split("#")[-1])
+                    if i == 0:
+                        syll_output = syllabify(model_out.strip().split("#")[-1])
+                        known_endings[structure.rhyme_scheme[line_i]] = syll_output[-1][-min(len(syll_output[-1]), 3):]
 
-        # print(prompt)
+        result_pairs = []
+        for result in temp_result:
+            result_pairs.append((','.join(result), structure))
+            for line in result:
+                print(line)
+            print()
 
-        inputs = tokenizer(prompt, return_tensors="pt") 
-        tokenizer.encode(prompt, return_tensors="pt") #directly for input_ids
+        results_dict = evaluator.evaluate_outputs_structure(result_pairs)
 
-        # model output using Top-k sampling text generation method
-        sample_outputs = model.generate(**inputs,
-            do_sample=True,
-            pad_token_id=tokenizer.eos_token_id,
-            num_return_sequences=3,
-            penalty_alpha=0.6,
-            max_new_tokens=512,
-            stopping_criteria=StoppingSequenceCriteria(prompt, tokenizer),
-            )
+        for cat in results_dict:
+            print(f"{cat} -> {sum(results_dict[cat]) / len(results_dict[cat])}")
+        print()
+        print("=" * 30)
 
-        if dataset_type == DatasetType.SYLLABLES or dataset_type == DatasetType.SYLLABLES_AND_WORDS:
-            evaluator = Evaluator()
-
-        avg_length_ratio = 0
-        avg_syll_distance = 0
-        avg_syll_accuracy = 0
-        avg_end_accuracy = 0
-        avg_keyword_similarity = 0
-
-        # generated sequence
-        for i, sample_output in enumerate(sample_outputs):
-            model_out = tokenizer.decode(sample_output.tolist(), skip_special_tokens=True)
-            print("\n{}\n\n{}\n".format(i+1, model_out)) # tokenizer.decode(sample_output, skip_special_tokens=True)
-
-            if dataset_type == DatasetType.SYLLABLES or dataset_type == DatasetType.SYLLABLES_AND_WORDS:
-                length_ratio, syll_distance, syll_accuracy, end_accuracy, keyword_similarity = evaluator.eval_from_first_line(model_out)
-                print(f"length_ratio = {length_ratio}")
-                print(f"syll_distance = {syll_distance}")
-                print(f"syll_accuracy = {syll_accuracy}")
-                print(f"keyword_similarity = {keyword_similarity}")
-                # print(f"end_accuracy = {end_accuracy}")
-
-                avg_length_ratio += length_ratio
-                avg_syll_distance += syll_distance 
-                avg_syll_accuracy += syll_accuracy
-                # avg_end_accuracy += end_accuracy
-                avg_keyword_similarity += keyword_similarity
-
-
-    print("="*100)
-    print("\n\nepoch average:\n")
-    print(f"length_ratio = {avg_length_ratio / len(sample_outputs)}")
-    print(f"syll_distance = {avg_syll_distance / len(sample_outputs)}")
-    print(f"syll_accuracy = {avg_syll_accuracy / len(sample_outputs)}")
-    print(f"keyword_similarity = {avg_keyword_similarity / len(sample_outputs)}")
-    # print(f"end_accuracy = {avg_end_accuracy / len(sample_outputs)}")
-    print("="*100)
-    print("\n")
 
