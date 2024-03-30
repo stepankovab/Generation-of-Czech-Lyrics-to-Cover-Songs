e6b02749dff0db0b7ca923b26f26c88ac5009363
Barbora Stepankova <barca-stepankova@seznam.cz>
2024-03-30 21:15:28 +0100

    implement few-shot generating function for pipeline

---
+++ CODE/LLMExperiments/fewshot.py
@@ -0,0 +1,158 @@
+import torch
+import logging
+import random
+from lyrics_datasets import WholeLyricsDataset, prepare_prompt_whole, extract_output_whole
+from transformers import AutoTokenizer, AutoModelForCausalLM, StoppingCriteria
+from dataset_types import DatasetType
+from rhymer_types import RhymerType
+from english_structure_extractor import SectionStructure
+from postprocessing import Postprocesser
+from evaluator import Evaluator
+
+
+class StoppingSequenceCriteria(StoppingCriteria):
+    def __init__(self, prompt, tokenizer, desired_lines):
+        self.prompt=prompt
+        self.tokenizer = tokenizer
+        self.desired_lines = desired_lines
+
+    def __call__(self, input_ids, scores, **kwargs):
+        generated_text = self.tokenizer.decode(input_ids[0])
+        generated_text = generated_text.replace(self.prompt,'')
+        generated_lines = generated_text.strip().split("\n")
+        if len(generated_lines) > self.desired_lines:
+            return True  # Stop generation
+        return False  # Continue generation
+
+    def __len__(self):
+        return 1
+
+    def __iter__(self):
+        yield self
+   
+
+def get_n_examples(dataset_path, dataset_type, n):
+    a = WholeLyricsDataset(dataset_path, dataset_type)
+    random.seed(31)
+    random.shuffle(a.lyrics_list)
+    return '\n'.join(a.lyrics_list[:min(n, len(a.lyrics_list))]) + "\n"
+
+
+def fewshot_and_generate(args, input_sections):
+    dataset_type = DatasetType(args.dataset_type)
+    
+    device = 'cpu'
+    if torch.cuda.is_available():
+        print("cuda available.")
+        device = 'cuda'
+
+    if args.model == "GPT2_oscar":
+        args.model = "lchaloupsky/czech-gpt2-oscar"
+    elif args.model == "GPT2_czech_XL":
+        args.model = "BUT-FIT/Czech-GPT-2-XL-133k"
+    elif args.model == "tinyLlama":
+        args.model = "TinyLlama/TinyLlama-1.1B-intermediate-step-1431k-3T"
+
+    model, tokenizer = AutoModelForCausalLM.from_pretrained(args.model), AutoTokenizer.from_pretrained(args.model)
+    model.to(device)
+    print(f"loaded model: {args.model}")
+
+    n_examples = get_n_examples(args.dataset_path, dataset_type, args.nshot)
+    print(n_examples)
+    
+    structure = SectionStructure(rt=RhymerType(args.rhymer))
+    postprocesser = Postprocesser(evaluator=Evaluator(rt=RhymerType(args.rhymer)))
+
+    result_pairs = []
+
+    for input_section in input_sections:
+        print("before structure filling")
+        # Load the structure of the english text
+        structure.fill(input_section) 
+
+        print("after structure filling")
+
+        prompt = prepare_prompt_whole(dataset_type, structure)
+        print(prompt)
+        prompt = n_examples + prompt
+
+
+        inputs = tokenizer([prompt],return_token_type_ids=False, return_tensors="pt").to(device)
+    
+        logging.info("prompt length: ", inputs["input_ids"].shape)
+        output_ids = model.generate(**inputs,
+            do_sample=True,
+            top_p=0.95,
+            repetition_penalty=1.0,
+            temperature=0.8,
+            max_new_tokens=256,
+            num_return_sequences=args.out_per_gerenation,
+            pad_token_id=tokenizer.eos_token_id,
+            penalty_alpha=0.6,
+            stopping_criteria=StoppingSequenceCriteria(prompt, tokenizer, structure.num_lines),
+            )
+        
+        outputs = []
+        for o_ids in output_ids:
+            outputs.append(extract_output_whole(tokenizer.decode(o_ids.tolist(), skip_special_tokens=True), prompt, dataset_type, structure))
+        print(outputs)
+        print("after generation")
+        
+        print("before postprocessing")
+        model_out = postprocesser.choose_best_section(outputs, structure, remove_add_stopwords=args.postprocess_stopwords)
+        print("after postprocessing")
+        
+        print(f"\n{', '.join(model_out)}\n")
+
+        result_pairs.append((','.join(model_out), structure.copy()))
+        for line in model_out:
+            print(line)
+        print()
+
+    return result_pairs
+
+
+
+
+
+
+# simecek/cswikimistral_0.1
+# BUT-FIT/Czech-GPT-2-XL-133k
+# lchaloupsky/czech-gpt2-oscar
+# TinyLlama/TinyLlama-1.1B-intermediate-step-1431k-3T
+# BUT-FIT/CSTinyLlama-1.2B
+# BUT-FIT/csmpt7b
+# mistralai/Mistral-7B-v0.1
+
+
+
+
+
+
+
+
+
+
+# def get_3_shot_paragraph_prompt(sylls: str, keywords: list, ending: str):
+# #     return """
+# # ### System: Konverzace mezi člověkem a automatickým asistentem. Asistent pomáhá člověku vymýšlet větu o jednom řádku, která se rýmuje s poksytnutým slovem. Věta obsahuje poskytnutý počet slabik. Navíc asistent vymyslí větu co dává smysl podle poskytnutých klíčových slov.
+# # ### Člověk: Rým = pes, počet slabik = 9, klíčová slova = les voda
+# # ### Asistent: Za lesem u vody leží ves.
+# # ### Člověk: Rým = důl, počet slabik = 5, klíčová slova = stůl jídlo
+# # ### Asistent: Na stole je sůl.
+# # ### Člověk: Rým = vaz, počet slabik = 7, klíčová slova = zuby problém
+# # ### Asistent: A zase mám další kaz.
+# # ### Člověk: {}
+# # ### Asistent: 
+# # """.format(question)
+#     return """
+# ### System: Konverzace mezi člověkem a automatickým asistentem. Asistent pomáhá člověku vymýšlet větu o jednom řádku, která se rýmuje s poksytnutým slovem. Věta obsahuje poskytnutý počet slabik. Navíc asistent vymyslí větu co dává smysl podle poskytnutých klíčových slov.
+# ### Člověk: Napiš řádku na 9 slabik o 'les, voda', ať končí na 'ves'
+# ### Asistent: Za lesem u vody leží ves.
+# ### Člověk: Napiš řádku na 5 slabik o 'stůl, jídlo', ať končí na 'důl'
+# ### Asistent: Na stole je sůl.
+# ### Člověk: Napiš řádku na 7 slabik o 'zuby, problém', ať končí na 'vaz'
+# ### Asistent: A zase mám další kaz.
+# ### Člověk: Napiš řádku na {} slabik o '{}', ať končí na '{}'
+# ### Asistent: 
+# """.format(sylls, ', '.join(keywords), ending)
