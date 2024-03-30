e9e0157dcdebed3197f4807de23b512ef73cee65
Barbora Stepankova <barca-stepankova@seznam.cz>
2024-03-30 21:18:46 +0100

    integrate few-shot, add arguments

--- CODE/LLMExperiments/pipeline.py
+++ CODE/LLMExperiments/pipeline.py
@@ -3,29 +3,33 @@ from HT_loader import HT_loader
 import argparse
 from generate_whole import generate_whole
 from generate_lines import generate_lines
+from fewshot import fewshot_and_generate
 from rhymer_types import RhymerType
 import time
 
 parser = argparse.ArgumentParser()
-parser.add_argument("--batch_size", default=32, type=int, help="Batch size")
 parser.add_argument("--model", default="GPT2_oscar", type=str, help="tinyLlama # GPT2_oscar # GPT2_czech_XL # Mistral_czech # ") 
-parser.add_argument("--epoch", default=3, type=int, help="Epoch of the trained model")
-parser.add_argument("--dataset_path", default="./", type=str, help="./ # DATA/Velky_zpevnik ")
 parser.add_argument("--model_path", default="./CODE/LLMExperiments/trained_models", type=str, help="./ #  CODE/LLMExperiments/trained_models")
-parser.add_argument("--input_section", default="let it go,let it go,can't hold it back anymore,turn away and slam the door", type=str, help="Input section in English")
-parser.add_argument("--from_dict", default=False, type=bool, help="Take testing data from HT dict")
+parser.add_argument("--input_section", default="why is it not working,i am sad,this is so bad", type=str, help="Input section in English, lines divided by comma ',' and sections divided by semicolon ';' -> eg: let it go,let it go,can't hold it back anymore,turn away and slam the door")
 parser.add_argument("--dataset_type", default=5, type=int, help="Dataset type: BASELINE = 1, SYLLABLES = 2, END_OF_LINES = 3, CHARACTERISTIC_WORDS = 4, UNRHYMED = 5, SYLLABLES_AND_WORDS = 6, SYLLABLES_AND_ENDS = 7, ENDS_AND_WORDS = 8")
-parser.add_argument("--generation_method", default="lines", type=str, help="whole, lines")
+parser.add_argument("--from_dict", default=False, type=bool, help="Take testing data from HT dict")
+parser.add_argument("--test_set_size", default=1000, type=int, help="How many samples from test set to take, 0 means all")
+parser.add_argument("--dataset_path", default="./", type=str, help="./ # DATA/Velky_zpevnik ")
+parser.add_argument("--epoch", default=3, type=int, help="Epoch of the trained model")
+parser.add_argument("--generation_method", default="fewshot", type=str, help="whole, lines, fewshot")
 parser.add_argument("--out_per_gerenation", default=10, type=int, help="number of generated outputs to choose the best from")
+parser.add_argument("--nshot", default=10, type=int, help="Number of examples when using few-shot as generation method")
 parser.add_argument("--rhymer", default=2, type=int, help="Rhymer type: RHYMETAGGER = 1, RHYMEFINDER = 2, SAME_WORD_RHYMETAGGER = 3")
 parser.add_argument("--postprocess_stopwords", default=False, type=bool, help="Posrprocess each output by trying to correct the length by removing/adding stopwords")
 args = parser.parse_args([] if "__file__" not in globals() else None)
 
 ########### get input data ###########
 if args.from_dict == True:
-    input_sections = HT_loader(args.dataset_path)
+    input_sections = HT_loader(args.dataset_path, language="en", without_repetition=True, shuffle=True)
+    if args.test_set_size > 0:
+        input_sections = input_sections[:min(args.test_set_size, len(input_sections))]
 else:
-    input_sections = [args.input_section, "this is a test, so try your best"]
+    input_sections = args.input_section.split(';')
 
 ########### generate outputs #############
 gen_start_stamp = time.time()
@@ -33,6 +37,8 @@ if args.generation_method == "whole":
     result_pairs = generate_whole(args, input_sections)
 elif args.generation_method == "lines":
     result_pairs = generate_lines(args, input_sections)
+elif args.generation_method == "fewshot":
+    result_pairs = fewshot_and_generate(args, input_sections)
 gen_end_stamp = time.time()
 
 ########### evaluate outputs ##############
